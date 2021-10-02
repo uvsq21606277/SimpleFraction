df0b767062f09a6fc91fc14863f2ce2993c74d22
uvsq21606277 <tinhinane.berkani@ens.uvsq.fr>
2021-10-02 10:40:37 +0200

    publier les modifications

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,3 +1,13 @@
 public class Fraction {
+    int x;
+    int y;
+    Fraction(int x,int y){
+        this.x=x;
+        this.y=y;
+    }
+    @Override
+    public String toString(){
+        return ("Fraction de " + this.x + "sur" + this.y);
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Simple affichage");
+        Fraction f;
+        f=new Fraction(10,5);
+        System.out.println(f.toString());
     }
 }
